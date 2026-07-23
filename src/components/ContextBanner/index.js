84a6e3f84ad566a7498e67ba77b93e295584ee01
abellone-aper <abellone@aper.com>
2026-07-23 14:52:09 -0300

    ...

--- src/components/ContextBanner/index.js
+++ src/components/ContextBanner/index.js
@@ -4,11 +4,16 @@ export default function ContextBanner({ icon, sub, title, linkText, href = '#' }
   return (
     <div className="ctx-banner">
       <div className="ctx-banner-icon">
-        <img src={icon} alt="" width={24} height={24} />
+        <span
+          className="ctx-banner-icon-mask"
+          style={{ WebkitMaskImage: `url('${icon}')`, maskImage: `url('${icon}')` }}
+        />
       </div>
       <div className="ctx-banner-body">
-        <p className="ctx-banner-sub">{sub}</p>
-        <p className="ctx-banner-title">{title}</p>
+        <div className="ctx-banner-text">
+          <p className="ctx-banner-sub">{sub}</p>
+          <p className="ctx-banner-title">{title}</p>
+        </div>
         {linkText && <a href={href} className="ctx-banner-link">{linkText}</a>}
       </div>
     </div>
